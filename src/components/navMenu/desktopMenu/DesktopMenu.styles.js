7c178e5c6a3ac0c2e0546a2e52e611c857becdec
Tyler Vance <tylervance@Tylers-MacBook-Pro.local>
2023-03-27 14:46:03 -0400

    Small changes

--- src/components/navMenu/desktopMenu/DesktopMenu.styles.js
+++ src/components/navMenu/desktopMenu/DesktopMenu.styles.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 export const DesktopMenu = styled.div`
     display: flex;
     box-sizing: border-box;
+    height: 100vh;
     height: 100dvh;
     position: fixed;
     /* background-color: blue; */
